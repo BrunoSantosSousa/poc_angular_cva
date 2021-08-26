6a2d5244fa262c27e67007ec79bb96da3dc74c80
BrunoSantosSousa <bruno_santos_sousa@outlook.com>
2021-08-26 20:03:05 +0000

    feat: init timepicker component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TimepickerComponent } from './timepicker/timepicker.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TimepickerComponent
   ],
   imports: [
     BrowserModule
